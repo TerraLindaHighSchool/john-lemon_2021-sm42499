87287df6ed302bb36eb1ce1fd41db3943e63e957
sm42499 <89489372+sm42499@users.noreply.github.com>
2022-01-13 15:23:23 -0800

    tutorial 3 of john lemon completed

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody rb;
+    private Vector3 moveDirection;
+    private Quaternion rotation;
+    private bool isWalking;
+
+    [SerializeField] private float turnSpeed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        rotation = Quaternion.identity;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Get user input
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Use user input to set direction player will move
+        moveDirection.Set(horizontal, 0f, vertical);
+        moveDirection.Normalize();
+
+        // Set animator to walking or idle depending on user input
+        isWalking = !(Mathf.Approximately(horizontal, 0f) && Mathf.Approximately(vertical, 0f));
+        animator.SetBool("IsWalking", isWalking);
+
+        // Assign rotation towards move direction
+        Vector3 desiredDirection = Vector3.RotateTowards(transform.position, moveDirection, turnSpeed = Time.deltaTime, 0f);
+        rotation = Quaternion.LookRotation(desiredDirection);
+    }
+    // Animator event 
+    private void OnAnimatorMove()
+    {
+        rb.MovePosition(rb.position + moveDirection * animator.deltaPosition.magnitude);
+        rb.MoveRotation(rotation);
+
+    }
+}
+
+   
