462565435e1bf5e50be615a06a4b3f58868e8872
sm42499 <89489372+sm42499@users.noreply.github.com>
2022-01-25 15:29:43 -0800

    enemies static almost completed

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -5,27 +5,24 @@ using UnityEngine;
 public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
     public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+
+    bool m_IsPlayerAtExit;
+    float m_Timer;
 
     void OnTriggerEnter (Collider other)
     {
         if (other.gameObject == player)
         {
-
-        }
-    }
-
-    void Update ()
-    {
-        if(m_IsPlayerAtExit)
-        {
-
+            m_IsPlayerAtExit = true;
         }
     }
 
     void EndLevel ()
     {
-        m_Timer++ = Time.deltaTime;
+        m_Timer+= Time.deltaTime;
 
         exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
 
@@ -43,6 +40,9 @@ public class GameEnding : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(m_IsPlayerAtExit)
+        {
+            EndLevel();
+        }
     }
 }
